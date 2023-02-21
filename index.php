f0eea519816a5b76a6fdab3896715609b948b4e8
Dissentleviathan <maghasp22@gmail.com>
2023-02-21 14:48:34 +0700

    1

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    // Ini adalah perintah untuk mencetak ke layar halaman.
+    echo "Hello World!";
+
+    /*
+        o> Ini adalah deklarasi variabel dengan tipe data.
+        o> Teks, bilangan bulet, bilangan desimal, boolean, dll.
+        o> Nama variabel diawali dengan huruf atau garis bawah.
+        o> Nama variabel hanya boleh ditulis dengan huruf atau garis bawah
+            a-z, A-Z, 0-9, dan garis bawah (_)
+        o> Nama variabel yang lebih dari satu kata dapat dipisahkan dengan garis bawah. Contoh : jenis_kelamin, gol_darah, ...
+    */
+    
+    $nama = "M Agha Setya Pratama";
+    $npm = "2125250072";
+    $jk = "Laki-Laki";
+    $umur = "21 Tahun";
+    $berat = "70 Kg";
+    $tinggi = "177 Cm";
+
+    echo "<br/>";
+    echo "Nama : ".$nama."<br/>";
+    echo "NPM : ".$npm."<br/>";
+    echo "Jenis Kelamin : ".$jk."<br/>";
+    echo "Umur : ".$umur."<br/>";
+    echo "Berat : ".$berat."<br/>";
+    echo "Tinggi : ".$tinggi."<br/>";
+
+    
+
+?>
